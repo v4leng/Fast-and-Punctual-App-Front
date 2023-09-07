93d2e10a79a2008399477609b296ba5ceba95d11
v4leng <valem.g_13@hotmail.com>
2023-09-07 14:36:23 -0300

    prueba

--- fast-and-punctual/src/app/page.tsx
+++ fast-and-punctual/src/app/page.tsx
@@ -103,9 +103,6 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
         </a>
       </div>
     </main>
